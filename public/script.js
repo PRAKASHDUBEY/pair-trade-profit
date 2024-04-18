90319cb5f1ee50eb73747f3fc168afcc9a7633b2
Prakash Dubey <prakashdubey@Prakashs-MacBook-Pro.local>
2024-04-18 13:13:37 +0530

    change api uri to cloud domain

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:8080";
-// const URL = "https://pair-trade.onrender.com"
+// const URL = "http://localhost:8080";
+const URL = "https://pair-trade.onrender.com"
 
 var info;
 
